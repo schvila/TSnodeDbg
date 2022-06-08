8738fd14b5236782a1c2d85fae41a4425c4c882f
Standa Chvila <schvila@gmail.com>
2022-06-08 05:16:28 +0200

    dubug configured.

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,8 @@ import http, { IncomingMessage, ServerResponse}   from 'http';
 const server = http.createServer((req: IncomingMessage, res: ServerResponse) => {
   console.log(req.method, req.url);
 });
+let i:number;
+i = 5;
 
+console.log(i, 'Listen on port 8080.');
 server.listen(8080);
